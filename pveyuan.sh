76eb1fdf21a90b5b84b02e8cd58b016ed535590b
sq1018 <149224031+sq1018@users.noreply.github.com>
2024-05-23 14:40:20 +0800

    Update pveyuan.sh

--- pveyuan.sh
+++ pveyuan.sh
@@ -2,8 +2,9 @@
 
 dir="/etc/apt/sources.list.d/"
 file="/etc/apt/sources.list"
+pve_file="/etc/apt/sources.list.d/pve-enterprise.list"
 
-if [ -d "$dir" ];; then
+if [ -d "$dir" ]; then
   echo "正在删除目录 $dir..."
   rm -rf "$dir"
   echo "目录已删除。"
@@ -20,3 +21,13 @@ echo "deb https://mirrors.ustc.edu.cn/debian-security bookworm-security main" >>
 echo "deb https://mirrors.ustc.edu.cn/proxmox/debian bookworm pve-no-subscription" >> "$file"
 
 echo "内容已替换。"
+
+if [ -f "$pve_file" ]; then
+  echo "正在修改 $pve_file..."
+  sed -i 's|^deb https://enterprise.proxmox.com/debian/pve|#deb https://enterprise.proxmox.com/debian/pve|' "$pve_file"
+  echo "添加清华源..."
+  echo "deb https://mirrors.tuna.tsinghua.edu.cn/proxmox/debian bullseye pve-no-subscription" >> "$pve_file"
+  echo "$pve_file 已修改。"
+else
+  echo "$pve_file 不存在，跳过修改。"
+fi
